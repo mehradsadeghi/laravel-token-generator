581aebbe304b3b19b596b2aa57852325794d6012
mehradsadeghi <2012.linkinpark@gmail.com>
2020-06-08 23:26:54 +0430

    fixing bug.

--- src/config/token-generator.php
+++ src/config/token-generator.php
@@ -7,21 +7,21 @@ return [
     'drivers' => [
 
         'basic' => [
-            'driver' => TokenGenerator\Drivers\Basic::class,
+            'driver' => \Mehradsadeghi\TokenGenerator\Drivers\Basic::class,
             'options' => [
                 'length' => 10
             ]
         ],
 
         'crypto' => [
-            'driver' => TokenGenerator\Drivers\Crypto::class,
+            'driver' => \Mehradsadeghi\TokenGenerator\Drivers\Crypto::class,
             'options' => [
                 'length' => 10
             ]
         ],
 
         'hash' => [
-            'driver' => TokenGenerator\Drivers\Hash::class,
+            'driver' => \Mehradsadeghi\TokenGenerator\Drivers\Hash::class,
             'options' => [
                 'alg' => 'sha256'
             ]
